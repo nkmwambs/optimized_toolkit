d0c3d41c9412dfe56bc6b4b077ae6d61430236b7
Nicodemus Karisa <nkmwambs@gmail.com>
2018-10-10 12:27:53 +0300

    Updated database SQLs to only procedures and schemas

--- database/storedprocedures.sql
+++ database/storedprocedures.sql
@@ -1,14 +1,24 @@
--- Adminer 4.6.3 MySQL dump
+DELIMITER $$
+CREATE PROCEDURE `get_cash_balances`(IN `icp` VARCHAR(6), IN `start_date` DATE)
+BEGIN
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SELECT accNo,amount FROM cashbal WHERE icpNo = icp AND month = start_date;
 
-DELIMITER ;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_accounts`;;
-CREATE  PROCEDURE `get_accounts`(IN `type` INT(5))
+DELIMITER $$
+CREATE PROCEDURE `get_cheque_details`(IN `icp` VARCHAR(6), IN `chq` INT(10))
+BEGIN
+
+SELECT Qty,Details,UnitCost,Cost,AccNo,scheduleID,civaCode,ChqNo,VNumber FROM voucher_body 
+WHERE ChqNo LIKE CONCAT(chq ,'-%') AND icpNo = icp;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `get_accounts`(IN `type` INT(5))
 BEGIN
 
 IF type IS NULL THEN 
@@ -16,33 +26,18 @@ SELECT * FROM accounts ORDER BY AccNo;
 ELSE 
 SELECT * FROM accounts WHERE AccGRP = type ORDER By AccNo;
 END IF;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_banks`;;
-CREATE  PROCEDURE `get_banks`()
-BEGIN
-
-SELECT * FROM banks;
-
-END;;
-
-DROP PROCEDURE IF EXISTS `get_cheques_utilized`;;
-CREATE  PROCEDURE `get_cheques_utilized`(IN `icp` VARCHAR(6))
+DELIMITER $$
+CREATE PROCEDURE `get_cheques_utilized`(IN `icp` VARCHAR(6))
 BEGIN
 SELECT ChqNo,sum(totals) as totals FROM voucher_header WHERE icpNo = icp AND VType='CHQ' GROUP BY ChqNo;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_cheque_details`;;
-CREATE  PROCEDURE `get_cheque_details`(IN `icp` VARCHAR(6), IN `chq` INT(10))
-BEGIN
-
-SELECT Qty,Details,UnitCost,Cost,AccNo,scheduleID,civaCode,ChqNo,VNumber FROM voucher_body 
-WHERE ChqNo LIKE CONCAT(chq ,'-%') AND icpNo = icp;
-
-END;;
-
-DROP PROCEDURE IF EXISTS `get_civs`;;
-CREATE  PROCEDURE `get_civs`(IN `status` VARCHAR(10))
+DELIMITER $$
+CREATE PROCEDURE `get_civs`(IN `status` VARCHAR(10))
 BEGIN
 
 IF status = "open" THEN 
@@ -55,105 +50,128 @@ SELECT * FROM civa WHERE open = 0;
 
 END IF;
 
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_cleared_effects`;;
-CREATE  PROCEDURE `get_cleared_effects`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_banks`()
+BEGIN
+
+SELECT * FROM banks;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `get_cleared_effects`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN
 SELECT voucher_header.VType,voucher_header.TDate,voucher_header.VNumber,voucher_header.Payee,voucher_header.Address,voucher_header.ChqNo,voucher_header.TDescription,voucher_header.ChqState,voucher_header.clrMonth,SUM(Cost) as Cost  
 FROM voucher_body LEFT JOIN voucher_header ON voucher_body.hID = voucher_header.hID WHERE voucher_header.icpNo = icp AND 
 voucher_body.VType IN ('CHQ','CR')  AND ChqState = 1 AND  clrMonth BETWEEN start_date AND end_date GROUP BY voucher_header.VNumber;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_current_budget`;;
-CREATE  PROCEDURE `get_current_budget`(IN `icp` VARCHAR(6), IN `fyr` INT(2))
+DELIMITER $$
+CREATE PROCEDURE `get_current_budget`(IN `icp` VARCHAR(6), IN `fyr` INT(2))
 BEGIN
 
 SELECT plansschedule.scheduleID,planheader.icpNo,planheader.fy,plansschedule.AccNo,plansschedule.totalCost,plansschedule.details,plansschedule.approved FROM planheader LEFT JOIN plansschedule ON planheader.planHeaderID=plansschedule.planHeaderID WHERE planheader.fy = fyr AND planheader.icpNo = icp AND plansschedule.approved = 2;
 
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_icp_max_voucher`;;
-CREATE  PROCEDURE `get_icp_max_voucher`(IN `icp` VARCHAR(6))
+DELIMITER $$
+CREATE PROCEDURE `get_icp_max_voucher`(IN `icp` VARCHAR(6))
 BEGIN
 SELECT TDate,Fy,VNumber FROM voucher_header WHERE hID = (SELECT max(hID) FROM voucher_header WHERE icpNo = icp);
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_journal_transactions`;;
-CREATE  PROCEDURE `get_journal_transactions`(IN `icpNo` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_journal_transactions`(IN `icpNo` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN
 SELECT voucher_header.VType,voucher_header.TDate,voucher_header.VNumber,voucher_header.Payee,voucher_header.Address,voucher_header.ChqNo,voucher_header.TDescription,
-voucher_body.AccNo,accounts.AccText,accounts.AccGrp,SUM(Cost) as Cost FROM voucher_body LEFT JOIN voucher_header ON voucher_body.hID = voucher_header.hID 
+voucher_body.AccNo,accounts.AccText,accounts.AccGrp,voucher_body.Qty,voucher_body.Details,voucher_body.UnitCost,SUM(Cost) as Cost,voucher_body.scheduleID,voucher_body.civaCode  FROM voucher_body 
+LEFT JOIN voucher_header ON voucher_body.hID = voucher_header.hID 
 LEFT JOIN accounts ON voucher_body.AccNo = accounts.AccNo 	
 WHERE voucher_header.icpNo = icpNo AND voucher_header.TDate BETWEEN start_date AND end_date 	
 GROUP BY voucher_header.VNumber,voucher_body.AccNo;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_max_report_submitted`;;
-CREATE  PROCEDURE `get_max_report_submitted`(IN `icp` VARCHAR(6))
+DELIMITER $$
+CREATE PROCEDURE `get_max_report_submitted`(IN `icp` VARCHAR(6))
 BEGIN
 SELECT * FROM opfundsbalheader WHERE balHdID = (SELECT max(balHdID) FROM opfundsbalheader WHERE icpNo = icp);
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_months_sum_per_account`;;
-CREATE  PROCEDURE `get_months_sum_per_account`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
-BEGIN 
-
-SELECT accID,accounts.AccNo,AccText,AccName,sum(Cost) as Cost,AccGrp,parentAccID,VType  FROM accounts LEFT JOIN voucher_body ON accounts.AccNo=voucher_body.AccNo WHERE icpNo = icp AND TDate BETWEEN start_date AND end_date AND AccGrp IN (0,1) GROUP BY AccNo ORDER BY AccNo;
-
-END;;
-
-DROP PROCEDURE IF EXISTS `get_months_sum_per_vtype`;;
-CREATE  PROCEDURE `get_months_sum_per_vtype`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
-BEGIN 
-
-SELECT sum(Cost) as Cost,VType  FROM voucher_body WHERE icpNo = icp AND TDate BETWEEN start_date AND end_date GROUP BY VType ORDER BY VType;
-
-END;;
-
-DROP PROCEDURE IF EXISTS `get_month_fund_balance`;;
-CREATE  PROCEDURE `get_month_fund_balance`(IN `icp` VARCHAR(6), IN `closeDate` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_month_fund_balance`(IN `icp` VARCHAR(6), IN `closeDate` DATE)
 BEGIN 
 
 SELECT accID,AccText,AccName,AccNo,amount FROM opfundsbalheader LEFT JOIN opfundsbal ON opfundsbalheader.balHdID=opfundsbal.balHdID LEFT JOIN accounts ON opfundsbal.funds=accounts.AccNo WHERE icpNo = icp AND closureDate = closeDate AND amount != 0;
 
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_outstanding_effects`;;
-CREATE  PROCEDURE `get_outstanding_effects`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_months_sum_per_account`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+BEGIN 
+
+SELECT accID,accounts.AccNo,AccText,AccName,sum(Cost) as Cost,AccGrp,parentAccID,VType  FROM accounts LEFT JOIN voucher_body ON accounts.AccNo=voucher_body.AccNo WHERE icpNo = icp AND TDate BETWEEN start_date AND end_date AND AccGrp IN (0,1) GROUP BY AccNo ORDER BY AccNo;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `get_months_sum_per_vtype`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+BEGIN 
+
+SELECT sum(Cost) as Cost,VType  FROM voucher_body WHERE icpNo = icp AND TDate BETWEEN start_date AND end_date GROUP BY VType ORDER BY VType;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `get_outstanding_effects`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN
 SELECT voucher_header.VType,voucher_header.TDate,voucher_header.VNumber,voucher_header.Payee,voucher_header.Address,voucher_header.ChqNo,voucher_header.TDescription,voucher_header.ChqState,voucher_header.clrMonth,SUM(Cost) as Cost  
 FROM voucher_body LEFT JOIN voucher_header ON voucher_body.hID = voucher_header.hID WHERE voucher_header.icpNo = icp AND 
 voucher_body.VType IN ('CHQ','CR')  AND (ChqState = 0 || (ChqState = 1 &&  clrMonth > end_date)) AND voucher_header.TDate <= end_date  GROUP BY voucher_header.VNumber;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_project_details`;;
-CREATE  PROCEDURE `get_project_details`(IN `icp` VARCHAR(6))
+DELIMITER $$
+CREATE PROCEDURE `get_project_details`(IN `icp` VARCHAR(6))
 BEGIN 
 
 SELECT * FROM projectsdetails WHERE icpNo = icp;
 
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_special_accounts_sum`;;
-CREATE  PROCEDURE `get_special_accounts_sum`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_special_accounts_sum`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN 
 
 SELECT accounts.AccNo,AccText,AccName,sum(Cost) as Cost,AccGrp  FROM accounts LEFT JOIN voucher_body ON accounts.AccNo=voucher_body.AccNo WHERE icpNo = icp AND TDate BETWEEN start_date AND end_date AND AccGrp NOT IN (0,1) GROUP BY AccNo ORDER BY AccNo;
 
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_vnumber_grouped_month_transaction`;;
-CREATE  PROCEDURE `get_vnumber_grouped_month_transaction`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_vnumber_grouped_month_transaction`(IN `icp` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN
 SELECT voucher_header.VType,voucher_header.TDate,voucher_header.VNumber,voucher_header.Payee,voucher_header.Address,voucher_header.ChqNo,voucher_header.TDescription,SUM(Cost) as Cost FROM voucher_body LEFT JOIN voucher_header ON voucher_body.hID = voucher_header.hID 
 LEFT JOIN accounts ON voucher_body.AccNo = accounts.AccNo 	
 WHERE voucher_header.icpNo = icp AND voucher_header.TDate BETWEEN start_date AND end_date 	
 GROUP BY voucher_header.VNumber;
-END;;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE IF EXISTS `get_voucher_transactions`;;
-CREATE  PROCEDURE `get_voucher_transactions`(IN `icpNo` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
+DELIMITER $$
+CREATE PROCEDURE `get_voucher_transactions`(IN `icpNo` VARCHAR(6), IN `start_date` DATE, IN `end_date` DATE)
 BEGIN
 select voucher_header.VType,voucher_header.TDate,voucher_header.VNumber,voucher_header.Payee,voucher_header.Address,voucher_header.ChqNo,voucher_header.TDescription,
 voucher_body.AccNo,accounts.AccText,accounts.AccGrp,voucher_body.Qty,voucher_body.Details,voucher_body.UnitCost,voucher_body.Cost,voucher_body.scheduleID,voucher_body.civaCode 
@@ -162,8 +180,5 @@ LEFT JOIN voucher_header ON voucher_header.hID=voucher_body.hID
 LEFT JOIN accounts ON accounts.AccNo=voucher_body.AccNo 
 WHERE voucher_header.icpNo = icpNo AND voucher_header.TDate BETWEEN start_date AND end_date;
 
-END;;
-
+END$$
 DELIMITER ;
-
--- 2018-10-04 21:47:27
--- database/tables_schema.sql
+++ database/tables_schema.sql
@@ -5,13 +5,279 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `access`;
-CREATE TABLE `access` (
-  `access_id` int(100) NOT NULL AUTO_INCREMENT,
-  `level` int(100) NOT NULL,
-  `apps_id` int(100) NOT NULL,
-  `status` int(5) NOT NULL,
-  PRIMARY KEY (`access_id`)
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `accID` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `parentID` int(200) unsigned NOT NULL,
+  `parentAccID` int(200) unsigned NOT NULL,
+  `AccName` varchar(100) NOT NULL,
+  `AccText` varchar(10) NOT NULL,
+  `AccNo` int(20) NOT NULL,
+  `AccGrp` int(2) NOT NULL,
+  `prg` int(2) NOT NULL,
+  `track` int(2) NOT NULL DEFAULT '0',
+  `budget` int(2) NOT NULL DEFAULT '1',
+  `Active` int(1) NOT NULL DEFAULT '1',
+  `is_admin` int(5) NOT NULL,
+  PRIMARY KEY (`accID`),
+  KEY `AccNo` (`AccNo`),
+  KEY `AccGrp` (`AccGrp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `banks`;
+CREATE TABLE `banks` (
+  `bankID` int(100) NOT NULL AUTO_INCREMENT,
+  `bankName` varchar(100) NOT NULL,
+  `swift_code` varchar(50) NOT NULL,
+  PRIMARY KEY (`bankID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `bssubmitted`;
+CREATE TABLE `bssubmitted` (
+  `bsID` int(100) NOT NULL AUTO_INCREMENT,
+  `bsKeys` varchar(50) NOT NULL,
+  `icpNo` varchar(10) NOT NULL,
+  `month` date NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bsID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `cashbal`;
+CREATE TABLE `cashbal` (
+  `balID` int(100) NOT NULL AUTO_INCREMENT,
+  `month` date NOT NULL,
+  `icpNo` varchar(6) NOT NULL,
+  `accNo` varchar(10) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`balID`),
+  KEY `month` (`month`),
+  KEY `icpNo` (`icpNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `cheque_book`;
+CREATE TABLE `cheque_book` (
+  `cheque_book_id` int(100) NOT NULL AUTO_INCREMENT,
+  `bankID` int(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `start_serial` int(10) NOT NULL,
+  `pages` int(10) NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cheque_book_id`),
+  KEY `bankID` (`bankID`),
+  CONSTRAINT `cheque_book_ibfk_1` FOREIGN KEY (`bankID`) REFERENCES `banks` (`bankID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `civa`;
+CREATE TABLE `civa` (
+  `civaID` int(100) NOT NULL AUTO_INCREMENT,
+  `accID` int(10) NOT NULL,
+  `AccNoCIVA` varchar(100) NOT NULL,
+  `AccTextCIVA` varchar(100) NOT NULL,
+  `allocate` longtext NOT NULL,
+  `open` int(1) NOT NULL DEFAULT '1',
+  `closureDate` date NOT NULL,
+  PRIMARY KEY (`civaID`),
+  KEY `AccNoCIVA` (`AccNoCIVA`),
+  KEY `AccTextCIVA` (`AccTextCIVA`),
+  KEY `accID` (`accID`),
+  KEY `open` (`open`),
+  KEY `closureDate` (`closureDate`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `clusters`;
+CREATE TABLE `clusters` (
+  `clusters_id` int(100) NOT NULL AUTO_INCREMENT,
+  `clusterName` varchar(100) NOT NULL,
+  PRIMARY KEY (`clusters_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `opfundsbal`;
+CREATE TABLE `opfundsbal` (
+  `balID` int(100) NOT NULL AUTO_INCREMENT,
+  `balHdID` int(100) NOT NULL,
+  `funds` varchar(50) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`balID`),
+  KEY `balHdID` (`balHdID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `opfundsbalheader`;
+CREATE TABLE `opfundsbalheader` (
+  `balHdID` int(100) NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(6) NOT NULL,
+  `totalBal` decimal(10,2) NOT NULL,
+  `closureDate` date NOT NULL,
+  `allowEdit` int(1) NOT NULL DEFAULT '1',
+  `submitted` int(1) NOT NULL DEFAULT '0',
+  `systemOpening` int(1) NOT NULL DEFAULT '0',
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`balHdID`),
+  KEY `closureDate` (`closureDate`),
+  KEY `icpNo` (`icpNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `oschqbf`;
+CREATE TABLE `oschqbf` (
+  `osBfID` int(11) NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(6) NOT NULL,
+  `ChqNo` varchar(50) NOT NULL,
+  `TDate` date NOT NULL,
+  `TDescription` varchar(100) NOT NULL DEFAULT 'None',
+  `VNumber` int(11) NOT NULL,
+  `totals` decimal(10,2) NOT NULL,
+  `chqState` int(1) NOT NULL,
+  `clrMonth` date NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`osBfID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `pfdetails`;
+CREATE TABLE `pfdetails` (
+  `pfID` int(3) unsigned NOT NULL AUTO_INCREMENT,
+  `fname` varchar(50) NOT NULL,
+  `oname` varchar(50) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`pfID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `planheader`;
+CREATE TABLE `planheader` (
+  `planHeaderID` int(100) NOT NULL AUTO_INCREMENT,
+  `fy` int(2) NOT NULL,
+  `icpNo` varchar(6) NOT NULL,
+  `smtp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`planHeaderID`),
+  KEY `fy` (`fy`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `plansrequests`;
+CREATE TABLE `plansrequests` (
+  `requestID` int(11) NOT NULL AUTO_INCREMENT,
+  `senderID` int(10) NOT NULL,
+  `scheduleID` int(10) NOT NULL,
+  `rqMessage` longtext,
+  `closed` int(1) DEFAULT '0',
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`requestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `plansschedule`;
+CREATE TABLE `plansschedule` (
+  `scheduleID` int(11) NOT NULL AUTO_INCREMENT,
+  `planHeaderID` int(11) NOT NULL,
+  `planType` int(1) NOT NULL DEFAULT '1',
+  `AccNo` int(10) NOT NULL,
+  `progStage` int(10) NOT NULL DEFAULT '2' COMMENT '1 = Early Childhood, 2 = Childhood, 3 = Youth',
+  `plan_item_tag_id` int(10) NOT NULL,
+  `details` varchar(100) NOT NULL,
+  `qty` int(5) NOT NULL,
+  `unitCost` double NOT NULL,
+  `often` int(10) NOT NULL,
+  `totalCost` double NOT NULL,
+  `validate` double NOT NULL,
+  `month_1_amount` double NOT NULL,
+  `month_2_amount` double NOT NULL,
+  `month_3_amount` double NOT NULL,
+  `month_4_amount` double NOT NULL,
+  `month_5_amount` double NOT NULL,
+  `month_6_amount` double NOT NULL,
+  `month_7_amount` double NOT NULL,
+  `month_8_amount` double NOT NULL,
+  `month_9_amount` double NOT NULL,
+  `month_10_amount` double NOT NULL,
+  `month_11_amount` double NOT NULL,
+  `month_12_amount` double NOT NULL,
+  `notes` longtext NOT NULL,
+  `approved` int(1) NOT NULL DEFAULT '0' COMMENT '0-Draft,1=Submitted,2=Approved,3=Declined,4=Reinstated,5=Allow Delete',
+  `submitDate` date NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`scheduleID`),
+  KEY `planHeaderID` (`planHeaderID`),
+  KEY `approved` (`approved`),
+  KEY `AccNo` (`AccNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `projectsdetails`;
+CREATE TABLE `projectsdetails` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(5) NOT NULL,
+  `icpName` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cluster_id` int(10) NOT NULL,
+  `bankID` int(5) NOT NULL,
+  `system_start_date` date NOT NULL,
+  `status` int(2) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `statementbal`;
+CREATE TABLE `statementbal` (
+  `balID` int(11) NOT NULL AUTO_INCREMENT,
+  `month` date NOT NULL,
+  `statementDate` date NOT NULL,
+  `actualDate` date NOT NULL DEFAULT '0000-00-00',
+  `icpNo` varchar(6) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`balID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `transitfundsbf`;
+CREATE TABLE `transitfundsbf` (
+  `transitBfID` int(11) NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(6) NOT NULL,
+  `TDate` date NOT NULL,
+  `Details` varchar(100) NOT NULL DEFAULT 'None',
+  `VNumber` int(10) NOT NULL DEFAULT '0',
+  `amount` decimal(10,0) NOT NULL,
+  `TState` int(1) NOT NULL DEFAULT '0',
+  `clrMonth` date NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transitBfID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `varjustify`;
+CREATE TABLE `varjustify` (
+  `justifyID` int(100) NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(10) NOT NULL,
+  `AccNo` int(5) NOT NULL,
+  `Details` varchar(500) NOT NULL DEFAULT 'Notes not available',
+  `reportMonth` date NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`justifyID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `voucherfootnotes`;
+CREATE TABLE `voucherfootnotes` (
+  `footnoteID` int(100) NOT NULL AUTO_INCREMENT,
+  `hID` int(100) NOT NULL,
+  `VDate` date NOT NULL,
+  `icpNo` varchar(10) NOT NULL,
+  `VNumber` varchar(10) NOT NULL,
+  `msg` varchar(1000) NOT NULL,
+  `msg_from` varchar(100) NOT NULL,
+  `stmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`footnoteID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -27,6 +293,7 @@ CREATE TABLE `voucher_body` (
   `UnitCost` decimal(10,2) NOT NULL,
   `Cost` decimal(10,2) NOT NULL,
   `AccNo` varchar(20) NOT NULL,
+  `scheduleID` int(100) unsigned NOT NULL,
   `civaCode` int(10) NOT NULL DEFAULT '0',
   `VType` varchar(5) NOT NULL,
   `ChqNo` varchar(20) NOT NULL,
@@ -55,6 +322,33 @@ BEGIN
 END;;
 
 DELIMITER ;
+
+DROP TABLE IF EXISTS `voucher_body_trash`;
+CREATE TABLE `voucher_body_trash` (
+  `bID` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `hID` int(100) NOT NULL,
+  `icpNo` varchar(6) NOT NULL,
+  `VNumber` varchar(10) NOT NULL,
+  `TDate` date NOT NULL,
+  `Qty` decimal(10,2) NOT NULL,
+  `Details` text NOT NULL,
+  `UnitCost` decimal(10,2) NOT NULL,
+  `Cost` decimal(10,2) NOT NULL,
+  `AccNo` varchar(20) NOT NULL,
+  `civaCode` int(10) NOT NULL DEFAULT '0',
+  `VType` varchar(5) NOT NULL,
+  `ChqNo` varchar(20) NOT NULL,
+  `unixStmp` int(100) NOT NULL,
+  `tmStmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bID`),
+  KEY `icpNo` (`icpNo`),
+  KEY `VNumber` (`VNumber`),
+  KEY `TDate` (`TDate`),
+  KEY `hID` (`hID`),
+  KEY `VType` (`VType`),
+  KEY `AccNo` (`AccNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 DROP TABLE IF EXISTS `voucher_header`;
 CREATE TABLE `voucher_header` (
@@ -98,4 +392,32 @@ END;;
 
 DELIMITER ;
 
--- 2018-09-03 14:45:39
+DROP TABLE IF EXISTS `voucher_header_trash`;
+CREATE TABLE `voucher_header_trash` (
+  `hID` int(100) unsigned NOT NULL AUTO_INCREMENT,
+  `icpNo` varchar(6) NOT NULL,
+  `TDate` date NOT NULL,
+  `Fy` int(2) NOT NULL,
+  `VNumber` int(6) NOT NULL,
+  `Payee` varchar(200) NOT NULL,
+  `Address` varchar(200) NOT NULL,
+  `VType` varchar(50) NOT NULL,
+  `ChqNo` varchar(10) DEFAULT NULL,
+  `ChqState` int(5) NOT NULL,
+  `clrMonth` date NOT NULL,
+  `editable` int(5) NOT NULL DEFAULT '0',
+  `TDescription` text NOT NULL,
+  `totals` decimal(10,2) NOT NULL,
+  `reqID` int(100) NOT NULL DEFAULT '0',
+  `unixStmp` int(100) NOT NULL,
+  `tmStmp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`hID`),
+  KEY `TDate` (`TDate`),
+  KEY `icpNo` (`icpNo`),
+  KEY `Fy` (`Fy`),
+  KEY `VType` (`VType`),
+  KEY `VNumber` (`VNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2018-10-10 09:25:37
